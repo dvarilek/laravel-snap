b611fa06bc2880b48d18036307c1b706602af0a3
d1d3 <davan@outlook.cz>
2025-01-27 22:31:34 +0100

    feature: add latest and oldest methods to Snapshotable trait

--- src/Models/Concerns/Snapshotable.php
+++ src/Models/Concerns/Snapshotable.php
@@ -8,6 +8,7 @@ use Dvarilek\LaravelSnapshotTree\Services\Contracts\AttributeCollectorInterface;
 use Dvarilek\LaravelSnapshotTree\ValueObjects\SnapshotDefinition;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 /**
  * @mixin Model
@@ -15,16 +16,26 @@ use Illuminate\Database\Eloquent\Relations\MorphMany;
 trait Snapshotable
 {
 
-    // TODO: Implement
-
+    /**
+     * Configure what should and shouldn't be captured in a snapshot.
+     *
+     * @return SnapshotDefinition
+     */
     abstract public static function getSnapshotDefinition(): SnapshotDefinition;
 
     public function snapshot(): MorphMany
     {
-        return $this->morphMany(
-            Snapshot::class,
-            'origin',
-        );
+        return $this->morphMany(...$this->getPolymorphicRelationArguments());
+    }
+
+    public function latestSnapshot(): MorphOne
+    {
+        return $this->morphOne(...$this->getPolymorphicRelationArguments())->latest();
+    }
+
+    public function oldestSnapshot(): MorphOne
+    {
+        return $this->morphOne(...$this->getPolymorphicRelationArguments())->oldest();
     }
 
     /**
@@ -57,4 +68,19 @@ trait Snapshotable
         return $collector->collectAttributes($this, static::getSnapshotDefinition(), $extraAttributes);
     }
 
+    /**
+     * Return all arguments for polymorphic relation.
+     *
+     * @return array<string, mixed>
+     */
+    protected function getPolymorphicRelationArguments(): array
+    {
+        return [
+            'related' => config('snapshot-tree.snapshot-model.model'),
+            'name' => config('snapshot-tree.snapshot-model.morph_name'),
+            'type' => config('snapshot-tree.snapshot-model.morph-type'),
+            'id' => config('snapshot-tree.snapshot-model.morph-id'),
+            'localKey' => config('snapshot-tree.snapshot-model.local_key')
+        ];
+    }
 }
