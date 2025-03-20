0100e36bc912b6239d975bdac252e4b21034498d
dvarilek <davan@outlook.cz>
2025-03-20 10:58:52 +0100

    chore: add doc block to InvalidSnapshotException method

--- src/Exceptions/InvalidSnapshotException.php
+++ src/Exceptions/InvalidSnapshotException.php
@@ -9,12 +9,20 @@ use Illuminate\Database\Eloquent\Model;
 
 final class InvalidSnapshotException extends \InvalidArgumentException
 {
+    /**
+     * @param  Model&SnapshotContract $snapshot
+     * @param  Model $originModelCandidate
+     * @param  string $snapshotMorphTypeColumn
+     * @param  string $snapshotMorphKeyColumn
+     *
+     * @return self
+     */
     public static function invalidSnapshotMorph(
         SnapshotContract&Model $snapshot,
         Model $originModelCandidate,
         string $snapshotMorphTypeColumn,
         string $snapshotMorphKeyColumn
-    ): \InvalidArgumentException
+    ): self
     {
         return new self(sprintf(
             "The provided snapshot '%s' is not associated with model '%s' (ID: %s). Expected morph type '%s' and morph ID '%s', but found morph type '%s' and morph ID '%s'.",
